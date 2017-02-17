ea105e23cee11485743b8fa26182aaf3bfb82a5b
martygwilliams <martygwilliams@gmail.com>
2017-02-16 19:37:30 -0600

    Added code that I hope will connect the arduino to wifi
    Added code that I hope will connect the arduino to wifi. This only works for unencrypted networks and you'll have to modify the SSID of your network. Also, this library assumes that you're using a shield and you have to use 10, 11, 12, and 13 for the SPI connection to the HDG104 module. I don't have a wifi capable arduino yet, so I'll probably need to make changes once that comes in.

--- pumpcontroller.ino
+++ pumpcontroller.ino
@@ -11,11 +11,11 @@ soon.
 
 Also, the current version has tons of garbage in it that I've commented out. That will go away soon.
 */
-
+#include <WiFi.h>
 #include <NewPing.h>
 #define MOTOR_PIN 9
-#define TRIGGER_PIN  11
-#define ECHO_PIN     12
+#define TRIGGER_PIN  7
+#define ECHO_PIN     8
 #define MAX_DISTANCE 500
 
 unsigned int ping;
@@ -27,6 +27,8 @@ unsigned int systemstate=0;
 bool motorrunning=false;
 //char* systemstates[]={"system idle ","motor running ","high alarm ","no reading alarm "};
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+char ssid[] = "yourNetwork";     // the name of your network
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
 
 void setup() {
   // put your setup code here, to run once:
@@ -36,7 +38,22 @@ digitalWrite(MOTOR_PIN,HIGH);
 }
 
 void loop() {
+
   // put your main code here, to run repeatedly:
+  
+    // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present"); 
+    // don't continue:
+    while(true);
+  } 
+  
+ // attempt to connect to Wifi network:
+  while ( status != WL_CONNECTED) { 
+    Serial.print("Attempting to connect to open SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid);
+    
 delay(50);
 
 
@@ -117,6 +134,78 @@ if ((distancecm<alarmlimit)&&(distancecm!=0))
   Serial.println("cm");
  // digitalWrite(MOTOR_PIN,LOW);
 
+  // you're connected now, so print out the data:
+  Serial.print("You're connected to the network");
+  printCurrentNet();
+  printWifiData();
+}
+
+}
+
+void printWifiData() {
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+  Serial.println(ip);
+  Serial.println(ip);
+  
+  // print your MAC address:
+  byte mac[6];  
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  Serial.print(mac[5],HEX);
+  Serial.print(":");
+  Serial.print(mac[4],HEX);
+  Serial.print(":");
+  Serial.print(mac[3],HEX);
+  Serial.print(":");
+  Serial.print(mac[2],HEX);
+  Serial.print(":");
+  Serial.print(mac[1],HEX);
+  Serial.print(":");
+  Serial.println(mac[0],HEX);
+  
+  // print your subnet mask:
+  IPAddress subnet = WiFi.subnetMask();
+  Serial.print("NetMask: ");
+  Serial.println(subnet);
+
+  // print your gateway address:
+  IPAddress gateway = WiFi.gatewayIP();
+  Serial.print("Gateway: ");
+  Serial.println(gateway);
+}
+
+void printCurrentNet() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the MAC address of the router you're attached to:
+  byte bssid[6];
+  WiFi.BSSID(bssid);    
+  Serial.print("BSSID: ");
+  Serial.print(bssid[5],HEX);
+  Serial.print(":");
+  Serial.print(bssid[4],HEX);
+  Serial.print(":");
+  Serial.print(bssid[3],HEX);
+  Serial.print(":");
+  Serial.print(bssid[2],HEX);
+  Serial.print(":");
+  Serial.print(bssid[1],HEX);
+  Serial.print(":");
+  Serial.println(bssid[0],HEX);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+
+  // print the encryption type:
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption,HEX);
 }
 
 
