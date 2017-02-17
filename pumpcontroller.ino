927e202b648ecdb20d7f6ab2de702d3a739e3ee7
martygwilliams <martygwilliams@gmail.com>
2017-02-16 20:12:09 -0600

    Added a call to get time from an NTP server.
    Added a call to get time from an NTP server. 
    
    We just want to use this to try to schedule the pump to come on during the night to use electricity during off-peak hours.
    
    There's still no code in here to actually do anything with the time, waiting on the wifi enabled arduino. After I get the new board, I'll start plunking around with figuring out what to do with the time.
    
    Most of the added code came from https://www.arduino.cc/en/Tutorial/UdpNTPClient

--- pumpcontroller.ino
+++ pumpcontroller.ino
@@ -11,7 +11,9 @@ soon.
 
 Also, the current version has tons of garbage in it that I've commented out. That will go away soon.
 */
+#include <SPI.h>
 #include <WiFi.h>
+#include <WiFiUdp.h>
 #include <NewPing.h>
 #define MOTOR_PIN 9
 #define TRIGGER_PIN  7
@@ -29,12 +31,31 @@ bool motorrunning=false;
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 char ssid[] = "yourNetwork";     // the name of your network
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
+unsigned int localPort = 2390;      // local port to listen for UDP packets
+
+IPAddress timeServer(129, 6, 15, 28); // time.nist.gov NTP server
+
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+
+byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP Udp;
 
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(115200);
 pinMode(MOTOR_PIN, OUTPUT);
 digitalWrite(MOTOR_PIN,HIGH);
+  
+String fv = WiFi.firmwareVersion();
+if (fv != "1.1.0") 
+{
+Serial.println("Please upgrade the firmware");
+}
+
 }
 
 void loop() {
@@ -54,13 +75,93 @@ void loop() {
     Serial.println(ssid);
     status = WiFi.begin(ssid);
     
-delay(50);
+// wait 10 seconds for connection:
+delay(10000);
+ sendNTPpacket(timeServer); // send an NTP packet to a time server
+  // wait to see if a reply is available
+  delay(1000);
+  if (Udp.parsePacket()) {
+    Serial.println("packet received");
+    // We've received a packet, read the data from it
+    Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+    //the timestamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, esxtract the two words:
+
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+    Serial.print("Seconds since Jan 1 1900 = ");
+    Serial.println(secsSince1900);
+
+    // now convert NTP time into everyday time:
+    Serial.print("Unix time = ");
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    const unsigned long seventyYears = 2208988800UL;
+    // subtract seventy years:
+    unsigned long epoch = secsSince1900 - seventyYears;
+    // print Unix time:
+    Serial.println(epoch);
 
 
+    // print the hour, minute and second:
+    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
+    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
+    Serial.print(':');
+    if (((epoch % 3600) / 60) < 10) {
+      // In the first 10 minutes of each hour, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+    Serial.print(':');
+    if ((epoch % 60) < 10) {
+      // In the first 10 seconds of each minute, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.println(epoch % 60); // print the second
+  }
+  // wait ten seconds before asking for the time again
+  delay(10000);
+}
+
+// send an NTP request to the time server at the given address
+unsigned long sendNTPpacket(IPAddress& address) {
+  //Serial.println("1");
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  //Serial.println("2");
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  //Serial.println("3");
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  //Serial.println("4");
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  //Serial.println("5");
+  Udp.endPacket();
+  //Serial.println("6");
+  
+  //This begins the sonar ping code.
   ping=sonar.ping_median(200);
   distancecm=(sonar.convert_cm(ping));
 
-
+/*If the distance to the liquid is less than the alarm limit and not equal to 0(meaning no distance returned from sonar), 
+  turn on the pump, print some messages on serial and set the motorrunning variable to true.
+  */
 if ((distancecm<alarmlimit)&&(distancecm!=0))
   {
     digitalWrite(MOTOR_PIN,LOW);
@@ -70,6 +171,10 @@ if ((distancecm<alarmlimit)&&(distancecm!=0))
     //systemstate=2;
     
   }
+/*
+If the distance to the liquid is less than the upperlimit set point, not equal to 0(error condition) and the motorrunning variable is
+false, turn the motor on, print a serial message and set motorrunning variable to true.
+*/  
   else if ((distancecm<upperlimit)&&(distancecm!=0)&&(motorrunning==false))
   {
     digitalWrite(MOTOR_PIN,LOW);
@@ -77,6 +182,11 @@ if ((distancecm<alarmlimit)&&(distancecm!=0))
     motorrunning=true;
     //systemstate=1;
   }
+  
+/*
+If the distance to the liquid is less than the lowerlimit set point, not equal to 0(error condition) and the motorrunning variable is
+true, print a serial message and set motorrunning variable to true.
+*/  
    else if ((distancecm<lowerlimit)&&(distancecm!=0)&&(motorrunning==true))
   {
     //digitalWrite(MOTOR_PIN,LOW);
@@ -84,6 +194,11 @@ if ((distancecm<alarmlimit)&&(distancecm!=0))
     motorrunning=true;
     //systemstate=1;
   }
+  
+/*
+If the distance to the liquid is more than the upperlimit set point, not equal to 0(error condition) and the motorrunning variable is
+true, Turn the motor off, print a serial message and set motorrunning variable to false.
+*/ 
   else if ((distancecm>upperlimit)&&(distancecm!=0)&&(motorrunning==true))
   {
     digitalWrite(MOTOR_PIN,HIGH);
@@ -91,6 +206,10 @@ if ((distancecm<alarmlimit)&&(distancecm!=0))
    motorrunning=false;
     //systemstate=0;
   }
+  
+  /*
+If the distance is equal to 0(error condition), Turn the motor off, print a serial message and set motorrunning variable to false.
+*/ 
   else if (ping==0) 
   {
     Serial.print("No reading!");
@@ -99,12 +218,19 @@ if ((distancecm<alarmlimit)&&(distancecm!=0))
    motorrunning=false;
     //systemstate=3;
   }
+  
+  /*
+If the distance to the liquid is more than the upperlimit set point, not equal to 0(error condition) and the motorrunning variable is
+false, print a serial message.
+*/ 
  else if ((distancecm>upperlimit)&&(distancecm!=0)&&(motorrunning==false))
   {
     //digitalWrite(MOTOR_PIN,LOW);
     Serial.print("IDLE-Motor is not running");
     //systemstate=1;
   }
+  
+  //The logic should never get here. If it has, I've missed a possibility.
   else
   {
     Serial.print("Nothing to see here.");
